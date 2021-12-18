c841a6b53bbc76af27cde496d0feaf2366fea6f0
manalabePatrick <manalabepatrick@gmail.com>
2021-12-19 02:12:31 +0800

    create page ui modified

--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
+  questions:any = [];
+  question:any = {}
+  options:any = [];
+  option:string = '';
+
   constructor() { }
 
   ngOnInit() {
+      // console.log(String.fromCharCode(94 + 3)); convert number to letter, a starts at number 3
+  }
+
+  addQuestion(){
+
+    this.questions.push(this.question);
+  }
+
+  createQuiz(){
+    console.log(this.questions);
+  }
+
+  addOption(index){
+    // console.log(index);
+    this.options.push(this.option);
+    console.log(this.options);
   }
 
 }
